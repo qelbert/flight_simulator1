4d137e22dd666010a7d062a1a1812a5f59e267d6
Queue <elbertq2@gmail.com>
2020-08-25 20:17:13 -0500

    modified position and direction button functionality

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ function init () {
     paragraph = document.querySelector("p");
 
     // Put your code for the exercises here. (my code starts here)
-    button.addEventListener("click", function (event) {
+    button.addEventListener("click", function () {
       paragraph.innerHTML = "Houston, we have lift off!";
     });
 
 
-    missionAbort.addEventListener("mouseover", function (event) {
-        let element = event.target;
-        element.style.backgroundColor = "red";
-    });
+    // missionAbort.addEventListener("mouseover", function (event) {
+    //     let element = event.target;
+    //     element.style.backgroundColor = "red";
+    // }); // used hover css from LaunchCode class tutorial instead of this js code
 
 
-    missionAbort.addEventListener("click", function (event) {
+    missionAbort.addEventListener("click", function () {
       window.alert("Are you sure you want to abort the mission?");
     }); 
     // my code ends here
